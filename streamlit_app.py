5a86795654086214a3c73a2d4d5f68fda2748512
rajatchauhan99 <108617405+rajatchauhan99@users.noreply.github.com>
2023-11-11 09:34:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,16 @@ resolution_options = ["1920x1080", "1366x768", "1600x900", "3840x2160", "3200x18
 
 st.title("Laptop Price Predictor")
 
+st.write("<h5>In this project, I engineered a Laptop Price Predictor, leveraging a comprehensive dataset of 1303 laptop entries. The project involved intricate data manipulation and feature engineering. I employed essential libraries such as Pandas, NumPy, and Scikit-Learn for data preprocessing, cleaning, and machine learning.
+
+Data visualization was a pivotal step, and I harnessed Matplotlib and Seaborn to create insightful barplots and apply data transformations like log transformations for further analysis.
+
+To encode categorical variables, I employed a combination of One-Hot Encoding and SimpleImputer, facilitated by Scikit-Learn's OneHotEncoder and SimpleImputer. Further, I utilized Scikit-Learn's ColumnTransformer to efficiently transform and preprocess the data, allowing me to convert non-numeric data into a format suitable for machine learning models.
+
+For predictive modeling, I structured the process with Scikit-Learn's Pipelines, ensuring seamless and efficient data processing. I also applied Scikit-Learn's ensemble techniques, including stacking and voting classifiers, to boost prediction accuracy. What sets this project apart is the use of Pickle for model serialization, enabling the model to be saved and loaded effortlessly.
+
+The final model was deployed as a user-friendly web app using Flask, enabling users to estimate laptop prices based on various specifications.ython (Programming Language)</h5>", unsafe_allow_html=True)
+
 st.write("<h5>Select your desired configurations:</h5>", unsafe_allow_html=True)
 
 # Organize input elements in a single line
